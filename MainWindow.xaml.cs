d3f38ab7a0ed6282cb844248ac53282f5cd5d3fe
Alexandr123000 <alexandrt10000@gmail.com>
2026-02-07 20:03:40 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            char[] digits = new char[10] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }; // Added digits
+            int n = 0x221A;
+            string p = char.ConvertFromUtf32(n);
+        }
+    }
+}
